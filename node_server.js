822e72dadf0a78049a39c9410c740bcedfc35d90
Taylor Jones <jonest3@oregonstate.edu>
2017-08-06 02:41:58 -0500

    Updated node_server.js based on Max W.'s work
    Including view meals and deleting meals.

--- node_server.js
+++ node_server.js
@@ -22,7 +22,184 @@ var pool = mysql.createPool({
   password: '2302',
   database: 'cs340_jonest3',
 });
+// ---------- Searching Meals ------------
+app.get('/searchMeals', function(req,res,next){
+   res.render('searchMeals');
+});
 
+app.post('/searchMeals', function(req,res,next){
+   console.log(req.body);
+// name, genre, prep_time
+   var context = {};
+   if(req.body.name != null  ||  req.body.genre != null  ||  req.body.prep_time != null)
+   {
+        var queryString = "SELECT meal_id, name, description FROM meals WHERE";
+        var queryVars = [];
+        if(req.body.name != null)
+        {
+                queryString += " name LIKE '%"+req.body.name+"%'";
+        }
+
+        if(req.body.genre != null)
+        {
+                if(req.body.name != null)
+                        queryString += " AND";
+                queryString += " genre LIKE '%"+req.body.genre+"%'";
+        }
+
+        if(req.body.prep_time != null)
+        {
+                if(req.body.genre != null  ||  req.body.name != null)
+                         queryString += " AND";
+                queryString += " prep_time <="+req.body.prep_time;
+
+        }
+     
+        console.log(queryVars);
+        pool.query(queryString, function(err,rows,fields){
+                if(err)
+                {
+                        next(err);
+                        return;
+                }
+                res.send(JSON.stringify(rows));
+        });
+   }
+   else
+        res.send(JSON.stringify(0));
+});
+
+// --------- View Meal ------------
+app.post('/viewMeal', function(req, res, next){
+  var context = {};
+  var request = req.body;
+
+  //if request body contains a meal_id value, query all attributes of that meal
+    if (request.id) {
+        pool.query("SELECT `meal_id`, `name`, `description`, `genre`, `prep_time`, `image`, `username_id`, `restaurant_id` FROM meals WHERE `meal_id` = ?", [request.id], function(err, rows, fields){
+
+            if (err){
+              next(err);
+              return;
+            }
+
+            //save meal information to meal property
+            context.meal = rows[0];
+            console.log(context.meal);
+
+            pool.query("SELECT `ingredient_id`, `meal_id` FROM contains WHERE `meal_id` = ?", [request.id], function(err, rows, fields){
+
+                if (err) {
+                    next(err);
+                    return;
+                }
+
+                context.activeIngredients = rows;
+                //console.log(context.activeIngredients);
+
+                pool.query("SELECT `ingredient_id`, `name`, `type` FROM ingredients", function(err, rows, fields){
+
+                    if (err) {
+                        next(err);
+                        return;
+                    }
+
+                    context.ingredientsTable = rows;
+                    //console.log(context.ingredientsTable);
+                    context.activeIngredientsName = [];
+
+                    var restaurantInfo = context.meal
+                    //console.log(restaurantInfo.restaurant_id);
+
+                    for (x = 0; x < context.activeIngredients.length; x++) {
+
+                        for (y = 0; y < context.ingredientsTable.length; y++) {
+
+                            if (context.activeIngredients[x].ingredient_id == context.ingredientsTable[y].ingredient_id) {
+
+                                context.activeIngredientsName.push(context.ingredientsTable[y].name);
+                            }
+                        }
+                    }
+
+                    console.log(context.activeIngredientsName);
+
+                    if (Number.isInteger(restaurantInfo.restaurant_id)) {
+                        pool.query("SELECT `restaurant_id`, `name`, `street_name`, `city`, `state`, `zipcode` FROM restaurants WHERE `restaurant_id` = ?", [restaurantInfo.restaurant_id], function(err, rows, fields){
+
+                            if (err) {
+                              next(err);
+                              return;
+                            }
+
+                            context.restaurant = rows[0];
+
+                            /******************************
+                            *ADD LOGIC HERE TO SERVER DIFFERENT
+                            *PAGE IF USER IS ORIGINAL MEAL CREATOR (AND SIGNED IN)
+                            ******************************/
+                            //render view page for user
+                            res.render('view_meal_creator', context);
+
+                            //if user is not original recipe creator render page without delete link
+                            //res.render('view_meal_browser', context);
+                        });
+                    } else {
+
+                        //res.type(`text/plain`);
+                        //res.send(context);
+                        console.log(context);
+
+                        /******************************
+                        *ADD LOGIC HERE TO SERVER DIFFERENT
+                        *PAGE IF USER IS ORIGINAL MEAL CREATOR (AND SIGNED IN)
+                        ******************************/
+                        //render view page for user
+                        res.render('view_meal_creator', context);
+
+                        //if user is not original recipe creator render page without delete link
+                        //res.render('view_meal_browser', context);
+                    }
+                });
+            });
+        });
+    }
+});
+
+// --------- Delete Meal -------------
+app.get('/remove/meal', function(req, res, next){
+  var context = {};
+
+  var query_params = req.query;
+
+  if (req.query.id) {
+    //query the record to be edited, so we can prepopulate the form inputs with the current record attribute values
+    pool.query("DELETE FROM meals WHERE `meal_id` = ?", [req.query.id], function(err, rows, fields){
+
+      if (err){
+        next(err);
+        return;
+      }
+      //change success to true for evaluation client side
+      if (rows.affectedRows == 1) {
+          context.delete_success = true;
+          //console.log("its true");
+      } else {
+          context.delete_success = false;
+          //console.log("its false");
+      }
+
+      //send feedback to the user
+      if (context.delete_success == true) {
+          res.render('delete_success', context);
+      } else {
+          res.render('delete_failure', context);
+      }
+    });
+  }
+});
+
+// ----- Error Handling ------
 app.use(function(req,res){
         res.status(404);
         res.render('404');
