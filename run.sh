0bb349f9af54db5ba3086e0c1620c9d762f46152
livesite <you@livesite.com>
2017-08-15 04:16:24 +0000

    updating the run.sh, still not 100

--- run.sh
+++ run.sh
@@ -10,9 +10,13 @@
 #node server.js
 #
 ####END####
-CODEBASE="/home/lori/CS-361/maxwell-try-one"
+CODEBASE="/home/lori/live"
 
-
+# localhost/loopback
+sudo iptables -t nat -I OUTPUT -p tcp -d 127.0.0.1 --dport 80 -j REDIRECT --to-ports 3000
+# external routing
+sudo iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 3000
+ 
 #write to run log. 
 
 #cd
@@ -32,7 +36,18 @@ update_nodejs
 
 #blah
 npm install ./server.js
-sleep 30
+#sleep 30
 
 #check if forever caught it. 
-forever list | grep server.js
+
+forever list
+
+#at this point, either run > node server.js 
+# in the live directory, this is the fastest for dev. 
+# if you are going to leave the live site standing up 
+# use > forever -m 100 start server.js
+# to kill a forever started server.
+# > forever stop [number of the server, usually 0]
+# or if there are runaways or super dead ones. 
+# > forever killall (?syntax)
+
